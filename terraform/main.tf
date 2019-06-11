4444217ba79f5346ab9a400b0ffd22263a00f055
Lennart Jern <lennart.jern@elastisys.com>
2019-06-11 13:59:57 +0200

    Allow ingress on 80 and 443 to workers

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,12 @@ resource "exoscale_security_group_rules" "master-rules" {
     cidr_list = ["0.0.0.0/0"]
     ports     = ["22"]
   }
+  # HTTP(S)
+  ingress {
+    protocol  = "TCP"
+    cidr_list = ["0.0.0.0/0"]
+    ports     = ["80", "443"]
+  }
   # Allow all internal communication
   ingress {
     protocol                 = "TCP"
