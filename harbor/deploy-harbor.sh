dd5784f8c09ecfca469257130c6649376d7120eb
Erik Gunne <erik.gu@hotmail.com>
2019-07-16 13:41:34 +0200

    Added comments and instructions to harbort deploy script

--- harbor/deploy-harbor.sh
+++ harbor/deploy-harbor.sh
@@ -1,100 +1,53 @@
-#!/bin/bash
 
-# Dir where this script is located
-HARBOR_DIR="$(dirname "$(readlink -f "$0")")"
-SCRIPT_DIR="${HARBOR_DIR}/../scripts"
-WORKSPACE="${HARBOR_DIR}/.."
-CERT_FOLDER="staging-certs"
+# NOTE: Script cannot be run as is, but has to be run manually, step by step. 
 
-echo $HARBOR_DIR
-echo $SCRIPT_DIR
-echo $WORKSPACE
-echo $CERT_FOLDER
-
-
-mkdir -p ${CERT_FOLDER}/kube-system/certs
-
-sh ${SCRIPT_DIR}/initialize-cluster.sh staging-certs "Jenkins admin1 admin2"
-
-# Install the cert-manager CRDs **before** installing the chart
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
-# https://docs.cert-manager.io/en/latest/getting-started/install.html#installing-with-helm
-# Create the namespace for cert-manager
-kubectl create namespace cert-manager --dry-run -o yaml | kubectl apply -f -
-# Label the cert-manager namespace to disable resource validation
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true --overwrite
-# Add the Jetstack Helm repository
-helm repo add jetstack https://charts.jetstack.io
-# Update your local Helm chart repository cache
-helm repo update
-
-# add pod wait something
-
-helm upgrade cert-manager jetstack/cert-manager \
-    --install --namespace cert-manager --version v0.8.0
-# Create letsencrypt-staging and prod ClusterIssuers
-kubectl apply -f ${WORKSPACE}/manifests/issuers/letsencrypt-staging.yaml
-kubectl apply -f ${WORKSPACE}/manifests/issuers/letsencrypt-prod.yaml
-
-kubectl create ns harbor --dry-run -o yaml | kubectl apply -f -
-
-kubectl apply -f ${WORKSPACE}/manifests/podSecurityPolicy/psp-access.yaml
-
-kubectl -n harbor create rolebinding harbor-privileged-psp \
-    --clusterrole=psp:privileged --serviceaccount=harbor:default \
-    --dry-run -o yaml | kubectl apply -f -
-
-
-helm upgrade harbor ${WORKSPACE}/charts/harbor \
-  --install \
-  --namespace harbor \
-  --values ${WORKSPACE}/helm-values/harbor-values.yaml
-
-kubectl -n harbor annotate ingress harbor-harbor-ingress certmanager.k8s.io/cluster-issuer=letsencrypt-prod
-
-# helm upgrade harbor /home/erik/a1-demo/charts/harbor \
-#   --install \
-#   --namespace harbor \
-#   --values /home/erik/a1-demo/helm-values/harbor-values.yaml
-
-
-##############################################################
-
+# Export environement variables 
+# NOTE: TF_VAR_exoscale_api_key and TF_VAR_exoscale_secret_key have to be set in your environment
 export CERT_FOLDER=staging-certs
 export WORKSPACE=/home/erik/a1-demo
 
+# Prepare to create certificates 
 mkdir -p ${CERT_FOLDER}/kube-system/certs
-
 kubectl apply -f ${WORKSPACE}/manifests/podSecurityPolicy/psp-access.yaml
 
+# Start helm
 sh ${WORKSPACE}/scripts/initialize-cluster.sh staging-certs "helm admin1 admin2"
-
 source helm-env.sh kube-system staging-certs/kube-system/certs helm
 
-### NAMESPACES ####
+#
+# Namespaces 
+#
+
 # Create the namespace for cert-manager
 kubectl create namespace cert-manager --dry-run -o yaml | kubectl apply -f -
 
 # Create the namespace for harbor
 kubectl create ns harbor --dry-run -o yaml | kubectl apply -f -
 
-### LABELS ####
+#
+# Labels
+# 
+
 # Label the cert-manager namespace to disable resource validation
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true --overwrite
 
-### PSP ### 
-# Run 
+# Add additional PSP 
 kubectl apply -f ${WORKSPACE}/manifests/podSecurityPolicy/psp-access.yaml
 
-### Cert-manager ###
+#
+# Cert-manager 
+#
+
 # Install the cert-manager CRDs **before** installing the chart
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
-# https://docs.cert-manager.io/en/latest/getting-started/install.html#installing-with-helm
+
 # Add the Jetstack Helm repository
 helm repo add jetstack https://charts.jetstack.io
+
 # Update your local Helm chart repository cache
 helm repo update
 
+# Install cert-manager
 helm upgrade cert-manager jetstack/cert-manager \
     --install --namespace cert-manager --version v0.8.1
 
@@ -102,11 +55,22 @@ helm upgrade cert-manager jetstack/cert-manager \
 kubectl apply -f ${WORKSPACE}/manifests/issuers/letsencrypt-staging.yaml
 kubectl apply -f ${WORKSPACE}/manifests/issuers/letsencrypt-prod.yaml
 
+# 
+# Harbor 
+# 
+
+# Create PersistentVolume and claiming it for harbor persistence. 
+# NOTE: IP addresses of nodes have to be manually configured in storage.yaml  
+kubectl apply -f ${WORKSPACE}/harbor/storage.yaml
+kubectl apply -f ${WORKSPACE}/harbor/pv-claim.yaml
+
+# Create rolebindings for harbor
 kubectl -n harbor create rolebinding harbor-privileged-psp \
     --clusterrole=psp:privileged --serviceaccount=harbor:default \
     --dry-run -o yaml | kubectl apply -f -
 
-
+# Deploying harbor 
+# NOTE: TF_VAR_exoscale_api_key and TF_VAR_exoscale_secret_key have to be set in your environment
 helm upgrade harbor ${WORKSPACE}/charts/harbor \
   --install \
   --namespace harbor \
@@ -114,6 +78,5 @@ helm upgrade harbor ${WORKSPACE}/charts/harbor \
   --set persistence.imageChartStorage.s3.secretkey=$TF_VAR_exoscale_secret_key \
   --set persistence.imageChartStorage.s3.accesskey=$TF_VAR_exoscale_api_key
 
+# Annotate certmanager for harbor 
 kubectl -n harbor annotate ingress harbor-harbor-ingress certmanager.k8s.io/cluster-issuer=letsencrypt-prod
-
- kubectl get secret mags-tls -n cert-manager --export -o yaml | kubectl apply -n harbor -f -
--- local-storage/setup.sh
+++ local-storage/setup.sh
@@ -2,6 +2,11 @@
 
 # all persistent data with be inside /home2 in the pods.
 
-kubectl apply -f storge.yaml
+# Set up PersistentVolume
+kubectl apply -f storage.yaml
+
+# Set up PersistentVolumeClaim
 kubectl apply -f pv-claim.yaml
+
+# Example of deployment
 kubectl apply -f local-test.yaml
