0f356552c0bdefc85308228202cc5b3096d327a0
Emil Vannebäck <emil.vanneback@elastisys.com>
2019-08-22 11:32:11 +0200

    using assume role for aws part of TF

--- terraform/modules/kubernetes-cluster/dns.tf
+++ terraform/modules/kubernetes-cluster/dns.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::248119176842:role/a1-pipeline"
+  }
 }
 
 data "aws_route53_zone" "zone" {
