7c8c1d3ecaf3aaae2c2df54670c15889a49c226e
Emil Vannebäck <emil.vanneback@elastisys.com>
2019-08-26 17:17:59 +0200

    added dns mapping to ECK_DOMAIN automatically

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,6 @@
 # TODO replace ECK_SS_DOMAIN with info from terraform output
-: "${ECK_SS_DOMAIN:?Missing ECK_SS_DOMAIN}"
-: "${ECK_C_DOMAIN:?Missing ECK_C_DOMAIN}"
+#: "${ECK_SS_DOMAIN:?Missing ECK_SS_DOMAIN}"
+#: "${ECK_C_DOMAIN:?Missing ECK_C_DOMAIN}"
 
 if [[ "$CERT_TYPE" == "prod" ]];
 then export TLS_VERIFY="true"
@@ -10,3 +10,10 @@ then export TLS_VERIFY="false"
     export TLS_SKIP_VERIFY="true"
 else echo "CERT_TYPE should be set to prod or staging"; exit 1;
 fi
+
+SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
+cd ${SCRIPTS_PATH}/../terraform
+tf_out=$(terraform output -json)
+export ECK_C_DOMAIN=$(echo ${tf_out} | jq -r '.c_dns_name.value' | sed 's/[^.]*[.]//')
+export ECK_SS_DOMAIN=$(echo ${tf_out} | jq -r '.ss_dns_name.value' | sed 's/[^.]*[.]//')
+cd ${SCRIPTS_PATH}
