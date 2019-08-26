c9d86a29e592011901f318ec164327a401033786
Emil Vannebäck <emil.vanneback@elastisys.com>
2019-08-26 13:03:44 +0200

    changed nfs server image to ubuntu 19.04

--- terraform/modules/kubernetes-cluster/main.tf
+++ terraform/modules/kubernetes-cluster/main.tf
@@ -97,7 +97,7 @@ resource "exoscale_compute" "worker" {
 
 resource "exoscale_compute" "nfs" {
   display_name    = "${var.nfs_name}"
-  template        = "Linux Ubuntu 18.04 LTS 64-bit"
+  template        = "Linux Ubuntu 19.04 64-bit"
   size            = "Large"
   disk_size       = 50
   key_pair        = "${exoscale_ssh_keypair.ssh_key.name}"
