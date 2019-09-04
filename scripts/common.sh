58ef385bcf3a0b39eaf350b0dc488bad0f96c980
viktor_forsberg <viktor@elastisys.com>
2019-09-04 12:54:07 +0200

    common.sh can now be run from any path

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,8 +1,8 @@
-SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
-cd ${SCRIPTS_PATH}/../terraform
+SCRIPTS_PATH="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+pushd "${SCRIPTS_PATH}/../terraform/" > /dev/null
 tf_out=$(terraform output -json)
 export ECK_C_DOMAIN=$(echo ${tf_out} | jq -r '.c_dns_name.value' | sed 's/[^.]*[.]//')
 export ECK_SS_DOMAIN=$(echo ${tf_out} | jq -r '.ss_dns_name.value' | sed 's/[^.]*[.]//')
 : "${ECK_SS_DOMAIN:?Missing ECK_SS_DOMAIN}"
 : "${ECK_C_DOMAIN:?Missing ECK_C_DOMAIN}"
-cd ${SCRIPTS_PATH}
+popd > /dev/null
