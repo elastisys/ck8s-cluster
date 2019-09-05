23a91694cd2b6218841f8474191487e2fda192ec
Olle Larsson <olle@elastisys.com>
2019-09-05 09:01:15 +0200

    Cleaned up and improved test script

--- pipeline/test/k8s/check-nodes.sh
+++ pipeline/test/k8s/check-nodes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This checks the types of each node by looking at the labels that RKE uses: worker, controlplane, and etcd.
+set -e
+
+# This checks the types of each node by looking at the labels: worker, controlplane, and etcd.
 # It determines whether or not the desired number of masters/etcd and workers in the cluster is met.
 
 # Check that cluster type argument is set and valid.
@@ -10,7 +12,7 @@ then
     exit 1
 fi
 
-echo "Running test on the $1 cluster"
+echo "Running tests on the $1 cluster"
 
 if [ $1 == "system-services" ]
 then 
@@ -22,18 +24,12 @@ fi
 
 SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
 
-# Get desired numbers from terraform.
+# Get terraform output
 cd ${SCRIPTS_PATH}/../../../terraform
 tf_out=$(terraform output -json)
-
-# For whatever reason it was not always able to get the output..
-if [[ "$?" -ne 0 ]]
-then 
-    exit 1
-fi
-
 cd ${SCRIPTS_PATH}/
 
+# Get the desired number of each node type.
 desired_workers=($(echo ${tf_out} | jq -r ".${prefix}_worker_count.value" ))
 desired_controlplanes=1
 desired_etcds=1
@@ -41,51 +37,41 @@ desired_etcds=1
 # Get the lables of each node
 labels=$(kubectl get nodes -o json | jq ".items[].metadata.labels")
 
-# Check number workers
-echo "Checking workers"
-workers=$(echo "$labels" | jq 'values | select(."node-role.kubernetes.io/worker" == "true") | [."node-role.kubernetes.io/worker"]')
+# Get how many nodes of each type there currently are in the cluster.
+workers=($(echo "$labels" | jq ' values | select(."node-role.kubernetes.io/worker" == "true") | [."node-role.kubernetes.io/worker"] | add | values'));
+controlplanes=($(echo "$labels" | jq 'values | select(."node-role.kubernetes.io/controlplane" == "true") | [."node-role.kubernetes.io/controlplane"] | add | values'))
+etcds=($(echo "$labels" | jq 'values | select(."node-role.kubernetes.io/etcd" == "true") | [."node-role.kubernetes.io/etcd"] | add | values'))
 
-# We need to check this because list=[""] is evaluated to be empty while ${#list[@]}=1...
-if [[ -z ${workers} ]]
+success="true"
+
+function check_nodes () {
+    type=$1
+    desired=$2
+    shift; shift
+    nodes=("$@")
+
+    if [[ "$type" != "workers" && "$type" != "controlplanes" && "$type" != "etcds" ]]
+    then 
+        echo "Invalid node type, must be one of: 'workers', 'controlplanes', 'etcds'"
+        exit 1
+    fi
+
+    echo "Checking $type"
+
+    if [[ "${#nodes[@]}" -ne  "$desired" ]]
+    then 
+        echo -e "\tInvalid number of $type nodes are running\n\tDesired: $desired\n\tRunning: ${#nodes[@]}"
+        success="false"
+    else
+        echo -e "\t${#nodes[@]}/$desired $type are running."
+    fi
+}
+
+check_nodes "workers" "$desired_workers" "${workers[@]}" 
+check_nodes "controlplanes" "$desired_controlplanes" "${controlplanes[@]}"
+check_nodes "etcds"  "$desired_etcds" "${etcds[@]}"
+
+if [[ "$success" != "true" ]]
 then 
-    echo -e "\tInvalid number of workers are running\n\tDesired: $desired_workers\n\tRunning: 0"
-else
-    if [[ "${#workers[@]}" -ne  "$desired_workers" ]]
-    then 
-        echo -e "\tInvalid number of worker nodes are running\n\tDesired: $desired_workers\n\tRunning: ${#workers[@]}"
-    else
-        echo -e "\t${#workers[@]}/$desired_workers workers are running."
-    fi
-fi
-
-# Check number of controlplanes.
-echo "Checking controlplanes"
-controlplanes=$(echo "$labels" | jq 'values | select(."node-role.kubernetes.io/controlplane" == "true") | [."node-role.kubernetes.io/controlplane"]')
-
-if [[ -z ${controlplanes} ]]
-then
-    echo -e "\tInvalid number of controlplanes are running\n\tDesired: $desired_controlplanes\n\tRunning: 0"
-else
-    if [[ ${#controlplanes[@]} -ne "$desired_controlplanes" ]]
-    then 
-        echo -e "\tInvalid number of controlplanes are running\n\tDesired: $desired_controlplanes\n\tRunning: ${#controlplanes[@]}"
-    else
-        echo -e "\t${#controlplanes[@]}/$desired_controlplanes controlplanes are running."
-    fi
-fi
-
-# Check number of etcds.
-echo "Checking etcds"
-etcds=$(echo "$labels" | jq 'values | select(."node-role.kubernetes.io/etcd" == "true") | [."node-role.kubernetes.io/etcd"]')
-
-if  [[ -z "${etcds}" ]]
-then
-    echo -e "\tInvalid number of etcds are running\n\tDesired: $desired_etcds\n\tRunning: 0"
-else 
-    if [ ${#etcds[@]} -ne "$desired_etcds" ]
-    then 
-        echo -e "\tInvalid number of etcds are running\n\tDesired: $desired_etcds\n\tRunning: ${#etcds[@]}"
-    else 
-        echo -e "\t${#etcds[@]}/$desired_etcds etcds are running."
-    fi
+    exit 1
 fi
