b7ef3dd05b5e68404acb4a3c63b2ec9c4aaf3ab8
Olle Larsson <olle@elastisys.com>
2019-09-30 11:32:47 +0200

    Added unsued ip address as well.

--- terraform/exoscale/modules/kubernetes-cluster/variables.tf
+++ terraform/exoscale/modules/kubernetes-cluster/variables.tf
@@ -36,7 +36,7 @@ variable "ssh_pub_key_file" {}
 
 variable "public_ingress_cidr_whitelist" {
   type    = list(string)
-  default = ["194.132.164.168/32", "193.187.219.4/32"] # Elastisys office, A1 office
+  default = ["194.132.164.168/32", "194.132.164.169/32", "193.187.219.4/32"] # Elastisys office, A1 office
 }
 
 ## DNS
