0c9b11138aba9c67bb9023ad1037c1e2214dc380
Olle Larsson <olle@elastisys.com>
2019-12-17 14:03:13 +0100

    Added directory for rclone with manifests and configs to sync between remotes.

---
+++ rclone/sync_buckets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Fail if remotes and sync buckets is empty
+: "${REMOTE_SRC:?Missing REMOTE_SRC}"
+: "${REMOTE_SRC:?Missing REMOTE_DST}"
+: "${BUCKETS_TO_SYNC:?Missing BUCKETS_TO_SYNC}"
+
+readonly cmd="sync"
+extra_args="$EXTRA_ARGS"
+remote_src="$REMOTE_SRC"
+remote_dst="$REMOTE_DST"
+buckets="$BUCKETS_TO_SYNC"
+
+for bucket in $buckets; do
+    rclone "${cmd}" --log-level DEBUG ${extra_args} "${remote_src}:${bucket}" "${remote_dst}:${bucket}"
+done
