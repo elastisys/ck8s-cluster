8f9f72e3b0e475cf0351dc8a06b76e8a2656714b
Emil Vannebäck <emil.vanneback@elastisys.com>
2019-12-23 10:00:46 +0100

    removed default value from api-key

--- terraform/exoscale/variables.tf
+++ terraform/exoscale/variables.tf
@@ -2,15 +2,12 @@
 variable exoscale_api_key {
   description = "Either use .cloudstack.ini or this to set the API key."
   type        = string
-  default     = ""
 }
 
 variable exoscale_secret_key {
   description = "Either use .cloudstack.ini or this to set the API secret."
   type        = string
-  default     = ""
 }
-
 
 # Service cluster variables.
 variable ssh_pub_key_file_sc {
