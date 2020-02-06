80b48035a0bca2f3d861b091843a61123960e4ff
Emil Vannebäck <emil.vanneback@elastisys.com>
2020-02-06 15:00:12 +0100

    make versions.sh executable

--- pipeline/versions.sh
+++ pipeline/versions.sh
@@ -3,4 +3,3 @@ export KUBECONFIG=$(pwd)/kube_config_eck-wc.yaml
 ./release/get-versions.sh
 export ECK_KUBECONFIG=$(pwd)/kube_config_eck-sc.yaml
 ./release/get-versions.sh
-cp release/version.json "${GITHUB_WORKSPACE}/version.json"
