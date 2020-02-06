b562b27eb3d70ac8df10dc4ca04a41fd1a0b3a40
Emil Vannebäck <emil.vanneback@elastisys.com>
2020-02-06 16:20:49 +0100

    pipe version output to new file to fix permission bug

--- pipeline/versions.sh
+++ pipeline/versions.sh
@@ -3,3 +3,4 @@ export KUBECONFIG=$(pwd)/kube_config_eck-wc.yaml
 ./release/get-versions.sh
 export ECK_KUBECONFIG=$(pwd)/kube_config_eck-sc.yaml
 ./release/get-versions.sh
+cat release/version.json > "${GITHUB_WORKSPACE}/version.json"
