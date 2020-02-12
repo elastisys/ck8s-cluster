aff3d7dc4a3a672d0264da939a3b0b454e379f6d
viktor_forsberg <viktor@elastisys.com>
2020-02-12 15:38:46 +0100

    Removed unneccessary code in "cleanup-safespring-volumes"
    Will be squashed

--- pipeline/cleanup-safespring-volumes.sh
+++ pipeline/cleanup-safespring-volumes.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 # Script used by pipeline to cleanup volumes in safespring.
-# Can be used manually if the cluster is still intact,
-# kube_config to service cluster is in the current working directory, 
-# and arg1 is set to build-number
-# Run number can be found in first step of infra as GITHUB_RUN_ID.
+# Can be used manually if the cluster is still intact and
+# kube_config to service cluster is in the current working directory.
 set -e
-SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
 
-if [[ -z "$GITHUB_RUN_ID" ]]; then
-    if [[ -n "$1" ]]; then
-        export MANUAL=true
-        GITHUB_RUN_ID=$1
-    else 
-        echo "ERROR: GITHUB_RUN_ID is empty and no arg1 is set. One is required"
-        exit 1
-    fi
-fi
-
-export CLOUD_PROVIDER=safespring
-export ENVIRONMENT_NAME="pipeline-$GITHUB_RUN_ID"
-source ${SCRIPTS_PATH}/vault-variables.sh
-source ${SCRIPTS_PATH}/variables.sh
-
-if [[ "$MANUAL" == true ]]; then
-    export TF_VAR_ssh_pub_key_file_sc="not-needed"
-    export TF_VAR_ssh_pub_key_file_wc="not-needed"
-else
-    export VAULT_TOKEN=$(${SCRIPTS_PATH}/vault-token-get.sh)
-fi
-
-export CONFIG_PATH=$(pwd)
 export KUBECONFIG=$(pwd)/kube_config_eck-sc.yaml
 
 kubectl delete ns elastic-system harbor monitoring fluentd influxdb-prometheus
