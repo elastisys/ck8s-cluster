33d9f12d90094e5faaa007918c4f159bbe835552
Jakub Krzywda <jakub@elastisys.com>
2020-02-19 09:34:23 +0100

    all: Add S3_REGION_ADDRESS environment variable in provider specific configuration files

--- citycloud-common-env.sh
+++ citycloud-common-env.sh
@@ -8,7 +8,8 @@ export OS_AUTH_VERSION=3
 export OS_IDENTITY_API_VERSION=3
 export OS_PROJECT_ID=ba47a6b513b645dd94fa31ea5e2becbd
 export S3_REGION=fra1
-export S3_REGION_ENDPOINT=https://swift-fra1.citycloud.com:8080/
+export S3_REGION_ADDRESS=swift-fra1.citycloud.com:8080
+export S3_REGION_ENDPOINT=https://$S3_REGION_ADDRESS
 
 export ECK_OPS_DOMAIN=ops.${ENVIRONMENT_NAME}.elastisys.se
 export ECK_BASE_DOMAIN=${ENVIRONMENT_NAME}.elastisys.se
--- safespring-common-env.sh
+++ safespring-common-env.sh
@@ -6,7 +6,8 @@ export OS_PROJECT_NAME=${OS_PROJECT_NAME:-infra.elastisys.se}
 export OS_REGION_NAME=se-east-1
 export OS_PROJECT_ID=${OS_PROJECT_ID:-9f91e56185fb4f929c36430ac4bcbe6e}
 export S3_REGION=${S3_REGION:-sto1}
-export S3_REGION_ENDPOINT=${S3_REGION_ENDPOINT:-https://s3.sto1.safedc.net}
+export S3_REGION_ADDRESS=${S3_REGION_ADDRESS:-s3.sto1.safedc.net}
+export S3_REGION_ENDPOINT=https://$S3_REGION_ADDRESS
 
 export ECK_OPS_DOMAIN=ops.${ENVIRONMENT_NAME}.elastisys.se
 export ECK_BASE_DOMAIN=${ENVIRONMENT_NAME}.elastisys.se
