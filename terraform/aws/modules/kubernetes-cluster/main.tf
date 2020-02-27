66e63e3e8a105ac29eaa2100fa587e6c1322942c
Daniel Harr <daniel.harr@elastisys.com>
2020-02-27 14:18:50 +0100

    aws: IAM roles, policies, profiles for worker

--- terraform/aws/modules/kubernetes-cluster/main.tf
+++ terraform/aws/modules/kubernetes-cluster/main.tf
@@ -370,3 +370,78 @@ resource "aws_iam_role_policy" "master" {
   }
   EOF
 }
+
+resource "aws_iam_instance_profile" "worker" {
+  name = "${var.prefix}-worker"
+  role = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role" "worker" {
+  name = "${var.prefix}-worker-role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": { "Service": "ec2.amazonaws.com"},
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy" "worker" {
+  name = "${var.prefix}-worker-policy"
+  role = aws_iam_role.worker.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "s3:*",
+        "Resource": [
+          "arn:aws:s3:::kubernetes-*"
+        ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": "ec2:Describe*",
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "ec2:AttachVolume",
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "ec2:DetachVolume",
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": ["route53:*"],
+        "Resource": ["*"]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:BatchGetImage"
+        ],
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
