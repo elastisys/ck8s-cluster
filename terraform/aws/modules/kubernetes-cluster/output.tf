2aad15b0d509ffed2093755e27b9cbe863db8a96
Daniel Harr <daniel.harr@elastisys.com>
2020-02-27 14:32:41 +0100

    aws: initial k8s module output

---
+++ terraform/aws/modules/kubernetes-cluster/output.tf
@@ -0,0 +1,43 @@
+output "master_public_ips" {
+  value = [
+    for instance in aws_instance.master: instance.public_ip
+  ]
+}
+
+output "master_private_ips" {
+  value = [
+    for instance in aws_instance.master: instance.private_ip
+  ]
+}
+
+output "worker_public_ips" {
+  value = [
+    for instance in aws_instance.worker: instance.public_ip
+  ]
+}
+
+output "worker_private_ips" {
+  value = [
+    for instance in aws_instance.worker: instance.private_ip
+  ]
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.main_sn.id
+}
+
+output "subnet_cidr_blocks" {
+  value = aws_subnet.main_sn.cidr_block
+}
+
+output "master_internal_loadbalancer_fqdn" {
+  value = aws_lb.master_lb_internal.dns_name
+}
+
+output "master_external_loadbalancer_fqdn" {
+  value = aws_lb.master_lb_external.dns_name
+}
