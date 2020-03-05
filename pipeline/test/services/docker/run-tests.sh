8ab02eee8fbf70b12478f39243bdf966adbb4d8c
davidumea <dava.andersson@hotmail.com>
2020-03-05 14:24:06 +0100

    Added support for multiple customer/admin names

--- pipeline/test/services/docker/run-tests.sh
+++ pipeline/test/services/docker/run-tests.sh
@@ -4,7 +4,7 @@ set -e
 # Script that runs service cluster and worker cluster tests on a local container
 
 # Requests user inputs or exits script if variable does not exist
-if [ "$1" == "" ]; then
+if [ "${1}" == "" ]; then
   echo 'Error: no argument found'
   echo 'Insert the docker image tag as an argument when running the script (ex ./run-tests.sh v0.1.0-dev)'
   exit 2
@@ -23,7 +23,7 @@ fi
 if [[ -z "${ENVIRONMENT_NAME}" ]]; then
   echo 'Missing ENVIRONMENT_NAME variable, insert environment variable (ex user-test):'
   read ENV_NAME
-  export ENVIRONMENT_NAME=$ENV_NAME
+  export ENVIRONMENT_NAME="${ENV_NAME}"
 fi
 
 # run container with entrypoint script and env variables
@@ -32,11 +32,11 @@ docker run -it -v "${CK8S}:/ck8s" \
 --entrypoint=/ck8s/pipeline/test/services/docker/container-entrypoint.sh \
 -e CK8S=/ck8s \
 -e CONFIG_PATH=/ck8s-config \
--e ENVIRONMENT_NAME=$ENVIRONMENT_NAME \
--e CLOUD_PROVIDER=$CLOUD_PROVIDER \
--e CUSTOMER_NAMESPACES=$CUSTOMER_NAMESPACES \
--e CUSTOMER_ADMIN_USERS=$CUSTOMER_ADMIN_USERS \
--e ECK_OPS_DOMAIN=$ECK_OPS_DOMAIN \
--e ECK_BASE_DOMAIN=$ECK_BASE_DOMAIN \
--e ENABLE_CUSTOMER_GRAFANA=$ENABLE_CUSTOMER_GRAFANA \
-elastisys/ck8s-ops:$1
+-e ENVIRONMENT_NAME="${ENVIRONMENT_NAME}" \
+-e CLOUD_PROVIDER="${CLOUD_PROVIDER}" \
+-e CUSTOMER_NAMESPACES="${CUSTOMER_NAMESPACES}" \
+-e CUSTOMER_ADMIN_USERS="${CUSTOMER_ADMIN_USERS}" \
+-e ECK_OPS_DOMAIN="${ECK_OPS_DOMAIN}" \
+-e ECK_BASE_DOMAIN="${ECK_BASE_DOMAIN}" \
+-e ENABLE_CUSTOMER_GRAFANA="${ENABLE_CUSTOMER_GRAFANA}" \
+elastisys/ck8s-ops:"${1}"
