32cb7a09ef646a5522467a740bb213a3d0a2533d
Simon Kollberg <simonkollberg@gmail.com>
2020-03-06 14:32:06 +0100

    all: Remove unused scripts

--- help-stuff/get-elastic-pw.sh
+++
@@ -1,4 +0,0 @@
-#This script will get the password for elasticsearch/kibana
-#Uses the default kubeconfig
-
-kubectl get secret elasticsearch-es-elastic-user -n elastic-system -o=jsonpath='{.data.elastic}' | base64 --decode
