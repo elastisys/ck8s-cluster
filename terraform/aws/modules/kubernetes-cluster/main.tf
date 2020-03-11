43c8d58f55eca0e9ca391b06be151c5b07d13c93
Daniel Harr <daniel.harr@elastisys.com>
2020-03-11 11:29:03 +0100

    infra aws: removed whitelist on worker nodes
    Removed whitelist on port 80 and 443

--- terraform/aws/modules/kubernetes-cluster/main.tf
+++ terraform/aws/modules/kubernetes-cluster/main.tf
@@ -257,7 +257,7 @@ resource "aws_security_group_rule" "http" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = var.public_ingress_cidr_whitelist
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.worker_sg.id
 }
 
@@ -266,7 +266,7 @@ resource "aws_security_group_rule" "https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = var.public_ingress_cidr_whitelist
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.worker_sg.id
 }
 
