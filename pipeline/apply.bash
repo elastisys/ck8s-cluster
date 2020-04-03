b3ed9fffa2db6ac1fc7a9a4e309cdab0d0c1ebee
Lennart Jern <lennart.jern@elastisys.com>
2020-04-03 21:28:10 +0300

    pipeline: Fix kubeconfig path

--- pipeline/apply.bash
+++ pipeline/apply.bash
@@ -16,6 +16,6 @@ TF_CLI_ARGS_apply="-auto-approve" "${ck8s}" apply all
 # TODO: The GitHub Actions runner does not run as root. Chmodding for now.
 #       Would be nice to find a cleaner solution.
 
-chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_rke_sc.yaml"
-chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_rke_wc.yaml"
+chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_sc.yaml"
+chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_wc.yaml"
 chmod 644 "${CK8S_CONFIG_PATH}/customer/kubeconfig.yaml"
