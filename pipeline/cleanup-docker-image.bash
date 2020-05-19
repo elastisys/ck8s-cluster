a84125ac7a9faf9c051105bf445fd1c4970cb51f
viktor_forsberg <viktor@elastisys.com>
2020-05-19 08:46:04 +0200

    pipeline: ensure that docker image is always removed

---
+++ pipeline/cleanup-docker-image.bash
@@ -0,0 +1,22 @@
+# Remove Docker image GITHUB_SHA tag created by pipeline from Docker Hub
+
+if [ -z ${GITHUB_SHA+x} ]; then
+    echo "GITHUB_SHA not set, skipping Docker tag deletion." >&2
+else
+    : "${DOCKERHUB_USER:?Missing DOCKERHUB_USER}"
+    : "${DOCKERHUB_PASSWORD:?Missing DOCKERHUB_PASSWORD}"
+
+    echo "Deleting Docker Hub tag: ${GITHUB_SHA}" >&2
+
+    TOKEN=$(curl -s -X POST \
+            -H "Content-Type: application/json" \
+            -d '{"username": "'${DOCKERHUB_USER}'",
+                 "password": "'${DOCKERHUB_PASSWORD}'"}' \
+                "https://hub.docker.com/v2/users/login/" | \
+            jq -r .token)
+
+    curl -i -X DELETE \
+        -H "Accept: application/json" \
+        -H "Authorization: JWT ${TOKEN}" \
+        "https://hub.docker.com/v2/repositories/elastisys/ck8s-ops/tags/${GITHUB_SHA}/"
+fi
