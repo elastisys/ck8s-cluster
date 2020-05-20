0c9331eac043494a5a536539701fbb2731db5a15
Emil Vannebäck <emil.vanneback@elastisys.com>
2020-05-20 12:56:16 +0200

    pipeline: merge init and apply job

--- pipeline/apply.bash
+++ pipeline/apply.bash
@@ -17,12 +17,6 @@ ${here}/test/infrastructure/whitelist.sh "positive"
 whitelist_update "public_ingress_cidr_whitelist" "127.0.0.1"
 whitelist_update "api_server_whitelist" "127.0.0.1"
 
-TF_CLI_ARGS_apply="-auto-approve" "${ck8s}" apply all
-${here}/test/infrastructure/whitelist.sh "positive"
-
-whitelist_update "public_ingress_cidr_whitelist" "0.0.0.0"
-whitelist_update "api_server_whitelist" "0.0.0.0"
-
 TF_CLI_ARGS_apply="-auto-approve" "${ck8s}" apply infra
 ${here}/test/infrastructure/whitelist.sh "negative"
 
