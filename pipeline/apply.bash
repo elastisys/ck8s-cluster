4342acdb6094c55ab483f85edf4c5b8f0a458555
Simon Kollberg <simonkollberg@gmail.com>
2020-05-26 23:22:40 +0200

    bin: Remove ck8s apply infra|k8s and ck8s ssh
    Replaced by the GO CLI.

--- pipeline/apply.bash
+++ pipeline/apply.bash
@@ -3,21 +3,20 @@
 set -eu -o pipefail
 
 here="$(dirname "$(readlink -f "$0")")"
-ck8s="${here}/../bin/ck8s"
-bin_path="${here}/../bin"
+ck8s_bash="${here}/../bin/ck8s"
 test_path="${here}/test"
 
 source "${here}/common.bash"
-source "${bin_path}/common.bash"
 
 sops_pgp_setup
 
 terraform_setup
 
-TF_CLI_ARGS_apply="-auto-approve" "${ck8s}" apply all
+ck8s apply --cluster sc --auto-approve
+ck8s apply --cluster wc --auto-approve
+
 # TODO: The GitHub Actions runner does not run as root. Chmodding for now.
 #       Would be nice to find a cleaner solution.
-
 chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_sc.yaml"
 chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_wc.yaml"
 chmod 644 "${CK8S_CONFIG_PATH}/customer/kubeconfig.yaml"
@@ -38,11 +37,14 @@ TF_CLI_ARGS_apply="-auto-approve" \
 ${test_path}/infrastructure/nodeport-whitelist.sh "negative"
 
 # Test API server whitelist
+
 ${test_path}/infrastructure/whitelist.sh "positive"
 
 whitelist_update "public_ingress_cidr_whitelist" "127.0.0.1"
 whitelist_update "api_server_whitelist" "127.0.0.1"
 
-TF_CLI_ARGS_apply="-auto-approve" \
-    sops exec-env "${CK8S_CONFIG_PATH}/secrets.env" "${bin_path}/apply.bash infra tf"
+# TODO: --terraform-only?
+ck8s apply --cluster sc --auto-approve
+ck8s apply --cluster wc --auto-approve
+
 ${test_path}/infrastructure/whitelist.sh "negative"
