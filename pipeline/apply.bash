155cf6d6221eb063b03004e69120ee84c26f3476
Lennart Jern <lennart.jern@elastisys.com>
2020-06-03 14:56:40 +0300

    pipeline: Chmod kubeconfig before test
    This makes sure that the kubeconfig is usable by the destroy script even 
    if one of the tests fails.

--- pipeline/apply.bash
+++ pipeline/apply.bash
@@ -15,6 +15,12 @@ sops_pgp_setup
 terraform_setup
 
 TF_CLI_ARGS_apply="-auto-approve" "${ck8s}" apply all
+# TODO: The GitHub Actions runner does not run as root. Chmodding for now.
+#       Would be nice to find a cleaner solution.
+
+chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_sc.yaml"
+chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_wc.yaml"
+chmod 644 "${CK8S_CONFIG_PATH}/customer/kubeconfig.yaml"
 
 # Run smoke tests (simple deployment and LoadBalancer on supported cloud providers)
 # We only run this on WC as SC is thoroughly tested from all apps deployed there.
@@ -31,10 +37,3 @@ whitelist_update "api_server_whitelist" "127.0.0.1"
 TF_CLI_ARGS_apply="-auto-approve" \
     sops exec-env "${CK8S_CONFIG_PATH}/secrets.env" "${bin_path}/apply.bash infra tf"
 ${test_path}/infrastructure/whitelist.sh "negative"
-
-# TODO: The GitHub Actions runner does not run as root. Chmodding for now.
-#       Would be nice to find a cleaner solution.
-
-chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_sc.yaml"
-chmod 644 "${CK8S_CONFIG_PATH}/.state/kube_config_wc.yaml"
-chmod 644 "${CK8S_CONFIG_PATH}/customer/kubeconfig.yaml"
