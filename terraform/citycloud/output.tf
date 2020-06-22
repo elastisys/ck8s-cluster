1cc7451be0793f086d5131c25841568eb1dcd8fc
Fredrik Liv <fredrik.liv@elastisys.com>
2020-06-22 12:59:01 +0200

    infra citycloud: Removed unneccesary output and added output

--- terraform/citycloud/output.tf
+++ terraform/citycloud/output.tf
@@ -30,88 +30,21 @@ output "wc_loadbalancer_ips" {
   value = module.workload_cluster.loadbalancer_ips
 }
 
-output "ansible_inventory_sc" {
-  value = templatefile("${path.module}/../templates/inventory.tmpl", {
-    master_hosts   = <<-EOF
-%{for key, master in module.service_cluster.master_ips~}
-${key} ansible_host=${master.public_ip} private_ip=${master.private_ip}
-%{endfor~}
-EOF
-    masters        = <<-EOF
-%{for key, master in module.service_cluster.master_ips~}
-${key}
-%{endfor~}
-EOF
-    worker_hosts   = <<-EOF
-%{for key, worker in module.service_cluster.worker_ips~}
-${key} ansible_host=${worker.public_ip} private_ip=${worker.private_ip}
-%{endfor~}
-EOF
-    workers        = <<-EOF
-%{for key, worker in module.service_cluster.worker_ips~}
-${key}
-%{endfor~}
-EOF
-    cluster_name   = var.prefix_sc == "" ? "${terraform.workspace}-service-cluster" : var.prefix_sc
-    cloud_provider = "openstack"
-    cloud_config   = "/etc/kubernetes/cloud.conf"
-    cloud_provider_vars = {
-      lb_enabled             = true
-      lb_subnet_id           = module.service_cluster.subnet_id
-      lb_external_network_id = var.external_network_id
-      use_octavia            = true
-      secgroup_id            = module.service_cluster.cluster_secgroup_id
-    }
-    calico_mtu              = "1480"
-    loadbalancers           = ""
-    public_endpoint         = values(module.service_cluster.loadbalancer_ips)[0].public_ip
-    private_network_cidr    = ""
-    control_plane_endpoint  = values(module.service_cluster.loadbalancer_ips)[0].private_ip
-    control_plane_port      = ""
-    internal_lb_children    = ""
-    kubeadm_init_extra_args = ""
-  })
+# Security group id
+output "sc_secgroup_id" {
+  value = module.service_cluster.cluster_secgroup_id
 }
 
-output "ansible_inventory_wc" {
-  value = templatefile("${path.module}/../templates/inventory.tmpl", {
-    master_hosts   = <<-EOF
-%{for key, master in module.workload_cluster.master_ips~}
-${key} ansible_host=${master.public_ip} private_ip=${master.private_ip}
-%{endfor~}
-EOF
-    masters        = <<-EOF
-%{for key, master in module.workload_cluster.master_ips~}
-${key}
-%{endfor~}
-EOF
-    worker_hosts   = <<-EOF
-%{for key, worker in module.workload_cluster.worker_ips~}
-${key} ansible_host=${worker.public_ip} private_ip=${worker.private_ip}
-%{endfor~}
-EOF
-    workers        = <<-EOF
-%{for key, worker in module.workload_cluster.worker_ips~}
-${key}
-%{endfor~}
-EOF
-    cluster_name   = var.prefix_wc == "" ? "${terraform.workspace}-workload-cluster" : var.prefix_wc
-    cloud_provider = "openstack"
-    cloud_config   = "/etc/kubernetes/cloud.conf"
-    cloud_provider_vars = {
-      lb_enabled             = true
-      lb_subnet_id           = module.workload_cluster.subnet_id
-      lb_external_network_id = var.external_network_id
-      use_octavia            = true
-      secgroup_id            = module.workload_cluster.cluster_secgroup_id
-    }
-    calico_mtu              = "1480"
-    loadbalancers           = ""
-    public_endpoint         = values(module.workload_cluster.loadbalancer_ips)[0].public_ip
-    private_network_cidr    = ""
-    control_plane_endpoint  = values(module.workload_cluster.loadbalancer_ips)[0].private_ip
-    control_plane_port      = ""
-    internal_lb_children    = ""
-    kubeadm_init_extra_args = ""
-  })
+output "wc_secgroup_id" {
+  value = module.workload_cluster.cluster_secgroup_id
 }
+
+# Subnet id for loadbalancer
+output "sc_lb_subnet_id" {
+  value = module.service_cluster.subnet_id
+}
+
+output "wc_lb_subnet_id" {
+  value = module.workload_cluster.subnet_id
+}
+
