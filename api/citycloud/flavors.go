3647f99498143b0e75264116f4e06bc8ba1f3a86
Fredrik Liv <fredrik.liv@elastisys.com>
2020-06-23 11:26:07 +0200

    gocli citycloud: Added development flavor

--- api/citycloud/flavors.go
+++ api/citycloud/flavors.go
@@ -6,11 +6,11 @@ import (
 )
 
 const (
-	// FlavorMinimum TODO
-	FlavorMinimum api.ClusterFlavor = "minimum"
+	// FlavorDevelopment TODO
+	FlavorDevelopment api.ClusterFlavor = "dev"
 
-	// FlavorHA TODO
-	FlavorHA api.ClusterFlavor = "ha"
+	// FlavorProduction TODO
+	FlavorProduction api.ClusterFlavor = "prod"
 )
 
 // Default TODO
@@ -33,21 +33,49 @@ func Default(clusterType api.ClusterType, clusterName string) *Cluster {
 		tfvars: openstack.OpenstackTFVars{
 			PublicIngressCIDRWhitelist: []string{},
 			APIServerWhitelist:         []string{},
+
+			ExternalNetworkID:   "71b10496-2617-47ae-abbc-36239f0863bb",
+			ExternalNetworkName: "public-v4",
+
+			// TODO: We need to get rid of these.
+			AWSDNSZoneID:  "Z2STJRQSJO5PZ0", // elastisys.se
+			AWSDNSRoleARN: "arn:aws:iam::248119176842:role/a1-pipeline",
 		},
 	}
 }
 
-// Minimum TODO
-func Minimum(clusterType api.ClusterType, clusterName string) api.Cluster {
+// Development TODO
+func Development(clusterType api.ClusterType, clusterName string) api.Cluster {
 	cluster := Default(clusterType, clusterName)
 
-	// TODO
+	cluster.tfvars.MasterNamesSC = []string{"master-0"}
+	cluster.tfvars.MasterNameSizeMapSC = map[string]string{
+		"master-0": "89afeed0-9e41-4091-af73-727298a5d959", // 2 Core 4gb mem 50gb storage
+	}
+
+	cluster.tfvars.WorkerNamesSC = []string{"worker-0", "worker-1"}
+	cluster.tfvars.WorkerNameSizeMapSC = map[string]string{
+		"worker-0": "f6a5e4d3-203d-45c0-a36a-dc5538580e1a", // 4 core 16gb mem 50gb storage
+		"worker-1": "ecd976c3-c71c-4096-b138-e4d964c0b27f", // 4 core 8gb mem 50gb storage
+	}
+	cluster.tfvars.MasterNamesWC = []string{"master-0"}
+	cluster.tfvars.MasterNameSizeMapWC = map[string]string{
+		"master-0": "89afeed0-9e41-4091-af73-727298a5d959", // 2 core 4gb mem 50gb storage
+	}
+
+	cluster.tfvars.WorkerNamesWC = []string{"worker-0"}
+	cluster.tfvars.WorkerNameSizeMapWC = map[string]string{
+		"worker-0": "ecd976c3-c71c-4096-b138-e4d964c0b27f", // 4 core 8gb mem 50gb storage
+	}
+
+	cluster.tfvars.MasterAntiAffinityPolicySC = "anti-affinity"
+	cluster.tfvars.MasterAntiAffinityPolicyWC = "anti-affinity"
 
 	return cluster
 }
 
-// HA TODO
-func HA(clusterType api.ClusterType, clusterName string) api.Cluster {
+// Production TODO
+func Production(clusterType api.ClusterType, clusterName string) api.Cluster {
 	cluster := Default(clusterType, clusterName)
 
 	// TODO
--- client/testdata/citycloud.tfvars
+++ client/testdata/citycloud.tfvars
@@ -1,16 +1,20 @@
-prefix_sc                     = ""
-prefix_wc                     = ""
-master_names_sc               = ["master-0"]
-master_name_flavor_map_sc     = { master-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
-worker_names_sc               = ["worker-0", "worker-1"]
-worker_name_flavor_map_sc     = { worker-0 = "ea0dbe3b-f93a-47e0-84e4-b09ec5873bdf", worker-1 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
-master_names_wc               = ["master-0"]
-master_name_flavor_map_wc     = { master-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
-worker_names_wc               = ["worker-0"]
-worker_name_flavor_map_wc     = { worker-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
-public_ingress_cidr_whitelist = ["194.132.164.168/32", "194.132.164.169/32"]
-api_server_whitelist          = ["194.132.164.168/32", "194.132.164.169/32"]
-aws_dns_zone_id               = "Z2STJRQSJO5PZ0"
-aws_dns_role_arn              = "arn:aws:iam::248119176842:role/pipeline"
-external_network_id           = "2aec7a99-3783-4e2a-bd2b-bbe4fef97d1c"
-external_network_name         = "ext-net"
+prefix_sc                      = ""
+prefix_wc                      = ""
+master_names_sc                = ["master-0"]
+master_name_flavor_map_sc      = { master-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
+worker_names_sc                = ["worker-0", "worker-1"]
+worker_name_flavor_map_sc      = { worker-0 = "ea0dbe3b-f93a-47e0-84e4-b09ec5873bdf", worker-1 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
+master_names_wc                = ["master-0"]
+master_name_flavor_map_wc      = { master-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
+worker_names_wc                = ["worker-0"]
+worker_name_flavor_map_wc      = { worker-0 = "dc67a9eb-0685-4bb6-9383-a01c717e02e8" }
+master_anti_affinity_policy_sc = "anti-affinity"
+worker_anti_affinity_policy_sc = "soft-anti-affinity"
+master_anti_affinity_policy_wc = "anti-affinity"
+worker_anti_affinity_policy_wc = "soft-anti-affinity"
+public_ingress_cidr_whitelist  = ["194.132.164.168/32", "194.132.164.169/32"]
+api_server_whitelist           = ["194.132.164.168/32", "194.132.164.169/32"]
+aws_dns_zone_id                = "Z2STJRQSJO5PZ0"
+aws_dns_role_arn               = "arn:aws:iam::248119176842:role/pipeline"
+external_network_id            = "2aec7a99-3783-4e2a-bd2b-bbe4fef97d1c"
+external_network_name          = "ext-net"
