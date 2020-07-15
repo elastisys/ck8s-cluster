44c3a792392c0bbb5db79a5e9f2df533cca6e575
Fredrik Liv <fredrik.liv@elastisys.com>
2020-07-15 10:24:08 +0200

    pipeline: Splitted out cleanup to a separate job to prepare for release

--- pipeline/setup-terraform.bash
+++ pipeline/setup-terraform.bash
@@ -9,3 +9,15 @@ else
       token = "'"${TF_TOKEN}"'"
     }' > ~/.terraformrc
 fi
+
+# TODO Remove when this is fixed https://github.com/actions/upload-artifact/issues/38
+for file in ${CK8S_CONFIG_PATH}/.state/.terraform/plugins/linux_amd64/terraform-provider*; do
+  if [ -f ${file} ]; then
+    chmod +x ${file}
+  fi
+done
+for file in ${CK8S_CONFIG_PATH}/.state/.terraform-tfe/plugins/linux_amd64/terraform-provider*; do
+  if [ -f ${file} ]; then
+    chmod +x ${file}
+  fi
+done
