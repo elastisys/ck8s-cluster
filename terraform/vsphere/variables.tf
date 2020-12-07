26031295662c58cf198811712144d05e26b60517
Jakub Krzywda <jakub@elastisys.com>
2020-12-07 21:06:00 +0100

    Add vSphere support

---
+++ terraform/vsphere/variables.tf
@@ -0,0 +1,111 @@
+## Global ##
+
+variable prefix {
+  default = ""
+}
+
+variable "network" {
+  type = map
+  
+  default = {
+    service_cluster = "VM Network"
+  }
+}
+
+variable "ip_prefix" {
+  default = "51.89.210"
+}
+
+variable "ip_last_octet_start_number_master" {
+  default = "145"
+}
+
+variable "ip_last_octet_start_number_worker" {
+  default = "146"
+}
+
+variable "gateway" {
+  default = "51.89.210.158"
+}
+
+variable "dns_primary" {
+  default = "8.8.4.4"
+}
+
+variable "dns_secondary" {
+  default = "8.8.8.8"
+}
+
+variable "vsphere_datacenter" {
+  default = "pcc-145-239-249-136_datacenter869"
+}
+
+variable "vsphere_compute_cluster" {
+  default = "Cluster1"
+}
+
+variable "vsphere_datastore" {
+  default= "ssd-000850"
+}
+
+variable "vsphere_server" {
+  default = "pcc-145-239-249-136.ovh.uk"
+}
+
+variable "vsphere_hostname" {
+  default = "172.16.254.50"
+}
+
+variable "firmware" {
+  default = "bios"
+}
+
+variable "template_name" {
+  default = "ubuntu-bionic-18.04-cloudimg-20201125"
+}
+
+variable "prefix_sc" {
+  default = ""
+}
+
+variable "prefix_wc" {
+  default = ""
+}
+
+variable "ssh_pub_key" {
+  default = ""
+}
+
+## Master ##
+variable "master_count" {
+  default = "1"
+}
+variable "master_cores" {
+  default = 2
+}
+
+variable "master_memory" {
+  default = 4096
+}
+
+variable "master_disk_size" {
+  default = "20"
+}
+
+## Worker ##
+
+variable "worker_count" {
+  default = "2"
+  # sc = "2"
+  # wc = "1"
+}
+variable "worker_cores" {
+  default = 8
+}
+
+variable "worker_memory" {
+  default = 16000
+}
+variable "worker_disk_size" {
+  default = "40"
+}
